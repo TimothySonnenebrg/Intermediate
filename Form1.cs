9b01a9910d10fb7419d659e672962a14b885adce
TimothySonnenebrg <35744582+TimothySonnenebrg@users.noreply.github.com>
2018-11-13 20:22:10 -0600

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,58 +1,128 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using TJS.AutoRepair.BL;
+using TJS.AutoRepair.PL;
 
-namespace AutomobileCost
+namespace TJS.AutoRepair.UI
 {
     public partial class Form1 : Form
     {
+        private string filename = "Customers.xml";
+
+        private Type type = typeof(CustomerCollection);
+
+        private CustomerCollection customers = new CustomerCollection();
+
+        public CustomerCollection cc { get; set; }
+
         public Form1()
         {
             InitializeComponent();
-        }
 
-        private void btnClear_Click(object sender, EventArgs e)
-        {
-            txtGas.Text = "";  txtInsurance.Text = ""; txtLoan.Text = "";
-            txtMaint.Text = ""; txtOil.Text = ""; txtTires.Text = "";
-            lblCostDisplay.Text = "";
+
+            customers.OnCustomerAdded += ReBindCustomers;
+            customers.LoadFromDB();
+            //populate method
+           // customers.LoadTestCustomers();
+           // customers = DataAccess.LoadFromXML(filename, type) as CustomerCollection;
+            
+            //Refresh
+           // ReBindCustomers();
            
         }
 
-        private void btnCalculate_Click(object sender, EventArgs e)
+        private void btnAddCustomer_Click(object sender, EventArgs e)
         {
-            double dblGas; double dblInsurance; double dblLoan;
-            double dblMaint; double dblOil; double dblTires;
-            double dblCost;
+            Customer newCust = new Customer();
+            newCust.FirstName = txtFirstName.Text;
+            newCust.LastName = txtLastName.Text;
+            newCust.Phone = txtPhone.Text;
 
+            customers.Add(newCust);
+           // ReBindCustomers();
+           
+
+        }
+
+        private void ReBindCustomers()
+        {
+            customers.Sort();
+            lstCustomer.DataSource = null;
+            lstCustomer.DataSource = customers;
+
+
+        }
+
+        private void RebindCars(List<Car> cars)
+        {
+            lstCars.DataSource = null;
+            lstCars.DataSource = cars ;
+
+            lstCars.DisplayMember = "Display";
+        }
+
+        private void lstCustomer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Customer selected = lstCustomer.SelectedItem as Customer;
+
+            if (selected != null)
+            {
+                // MessageBox.Show(selected.Phone);
+                txtFirstName.Text = selected.FirstName;
+                txtLastName.Text = selected.LastName;
+                txtPhone.Text = selected.Phone;
+
+                RebindCars( selected.Cars);
+            }
+
+
+        }
+
+        private void btnUpdateCustomer_Click(object sender, EventArgs e)
+        {
+            Customer selected = lstCustomer.SelectedItem as Customer;
+
+            if (selected != null)
+            {
+                try
+                {
+
+                selected.FirstName = txtFirstName.Text;
+                selected.LastName = txtLastName.Text;
+                selected.Phone = txtPhone.Text;
+                }
+                catch (BlankNameException)
+                {
+
+                    MessageBox.Show("Nooo Blank ");
+                }
+
+            }
+            
+            ReBindCustomers();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            customers.Sort();
+            ReBindCustomers();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
             try
             {
-
-                dblGas = double.Parse(txtGas.Text);
-                dblInsurance = double.Parse(txtInsurance.Text);
-                dblLoan = double.Parse(txtLoan.Text);
-                dblMaint = double.Parse(txtMaint.Text);
-                dblOil = double.Parse(txtOil.Text);
-                dblTires = double.Parse(txtTires.Text);
-
-                dblCost = dblGas + dblInsurance + dblLoan + dblMaint
-                            + dblOil + dblTires;
-
-
-              lblCostDisplay.Text = dblCost.ToString("c");
-
+                DataAccess.SaveToXML("Customers.xml", typeof(CustomerCollection), customers);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-               MessageBox.Show("Please enter a number");
+               
+               MessageBox.Show(ex.Message);
+                e.Cancel = true;
             }
+            
         }
     }
 }
